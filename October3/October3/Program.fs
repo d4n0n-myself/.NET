77ca0e862b721605e38a6a1e0909e27d6aa2bbf9
d4n0n_myself <danon.sibaev@gmail.com>
2019-10-10 17:21:48 +0300

    better

--- October3/October3/Program.fs
+++ October3/October3/Program.fs
@@ -1,47 +1,62 @@
 ﻿namespace October3
 
 open System
-open Microsoft.Extensions.Logging
 
 module Program =
 
 let tryParseDouble (arg : string) =
     match Double.TryParse arg with
-    | (true,d) -> Some(d)
-    | _ -> None
+    | (true,d) -> d
+    | _ -> Double.NaN
    
-let add x y = x+y
-let sub x y = x-y
-let mul x y = x*y
-let div x y = x/y
+//let add x y = x+y
+let sub = fun (x:double, y:double) -> x - y
+let add = fun (x:double, y:double) -> x + y
+let mul = fun (x:double, y:double) -> x * y
+let div = fun (x:double, y:double) -> x / y
+//let mul x y = x*y
+//let div x y = x/y
         
-type MaybeBuilder() =
-    member this.Bind(x, f) =
-        match x with
-        | None -> None
-        | Some a -> f a
+//thisype MaybeBuilder() =
+//    member this.Bind(x, f) =
+//        match x with
+//        | None -> None
+//        | Some a -> f a
 
-let add =
-    fun (op : string) (v1: double option) (v2: double option) ->
-        if (v1.IsSome && v2.IsSome)
-        then 
-            if op = "+" then v1.Value + v2.Value
-            if op = "-" then v1.Value - v2.Value
-            if op = "*" then v1.Value * v2.Value
-            if op = "/" then v1.Value / v2.Value
-        else (double)0;
+//let add =
+//    fun (op : string) (v1: double option) (v2: double option) ->
+//        if (v1.IsSome && v2.IsSome)
+//        then 
+//            if op = "+" then v1.Value + v2.Value
+//            if op = "-" then v1.Value - v2.Value
+//            if op = "*" then v1.Value * v2.Value
+//            if op = "/" then v1.Value / v2.Value
+//        else
+//            0.0;
             
 [<EntryPoint>]
 let main args =
 
 Console.WriteLine "Retard Alert"
 let first = Console.ReadLine() |> tryParseDouble
+if first = Double.NaN then
+    Console.WriteLine("Некорректный ввод, нужно вводить цифру")
+    0
+else
+    
 let op = Console.ReadLine()
 let second = Console.ReadLine() |> tryParseDouble
 
-let maybe = MaybeBuilder()
-
-if op = "+" then Console.WriteLine("Result:" + (add first second).ToString())
-//if op = "*" then Console.WriteLine("Result:" + (add first second))
-//if op = "-" then Console.WriteLine("Result:" + (sub first second))
-//if op = "/" then Console.WriteLine("Result:" + (div first second))  
+if second = Double.NaN then
+    Console.WriteLine("Некорректный ввод, нужно вводить цифру")
+    0
+else    
+    
+//let maybe = MaybeBuilder()
+//maybe.Bind
+let result = add(first, second)
+if op = "+" then Console.WriteLine("Result:" + (add(first, second)).ToString())
+if op = "*" then Console.WriteLine("Result:" + (mul(first, second)).ToString())
+if op = "-" then Console.WriteLine("Result:" + (sub(first, second)).ToString())
+if op = "/" then Console.WriteLine("Result:" + (div(first, second)).ToString())
+0 
