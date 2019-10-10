1486e8f739457541929600c956c6cc988854f5cf
d4n0n_myself <danon.sibaev@gmail.com>
2019-10-10 17:38:24 +0300

    more fancyness

--- Mandelbrott/Mandelbrott/Program.fs
+++ Mandelbrott/Mandelbrott/Program.fs
@@ -8,38 +8,38 @@ let cMax = complex 1.0 1.0
 let cMin = complex -1.0 -1.0
 
 // принадлежность 
-let rec isInMandelbrotSet (z, c, iter, count) =
-    if (cMin < z) && (z < cMax) && (count < iter) then
-        isInMandelbrotSet (((z * z) + c), c, iter, (count + 1))
+let rec inMandelbrot (z, c, i, count) =
+    if (cMin < z) && (z < cMax) && (count < i) then
+        inMandelbrot (((z * z) + c), c, i, (count + 1))
     else count
 
-// отображение стандартной плоскости на новую плоскость
+// перенос стандартной плоскости на новую плоскость
 let scalingFactor s = s * 1.0 / 200.0
 
-let mapPlane (x, y, s, mx, my) =
-    let fx = ((float x) * scalingFactor s) + mx
-    let fy = ((float y) * scalingFactor s) + my
+let mapPlane (x, y, s, diffX, diffY) =
+    let fx = ((float x) * scalingFactor s) + diffX
+    let fy = ((float y) * scalingFactor s) + diffY
     complex fx fy
 
 // раскрасить точки, не относящиеся к множеству
-let colorize c =
+let addColor number =
     let random = new Random()
-    let r = (random.Next(16) * c) % 255
-    let g = (random.Next(16) * c) % 255
-    let b = (random.Next(16) * c) % 255
+    let r = (random.Next(16) * number) % 255
+    let g = (random.Next(16) * number) % 255
+    let b = (random.Next(16) * number) % 255
     Color.FromArgb(r, g, b)
 
-let createImage (s, mx, my, iter) =
-    let image = new Bitmap(400, 400)
-    for x = 0 to image.Width - 1 do
-        for y = 0 to image.Height - 1 do
-            let count = isInMandelbrotSet (Complex.Zero, (mapPlane (x, y, s, mx, my)), iter, 0)
-            if count = iter then
-                image.SetPixel(x, y, Color.Black)
+let createImage (s, navX, navY, iterations) =
+    let bitmap = new Bitmap(400, 400)
+    for x = 0 to bitmap.Width - 1 do
+        for y = 0 to bitmap.Height - 1 do
+            let count = inMandelbrot (Complex.Zero, (mapPlane (x, y, s, navX, navY)), iterations, 0)
+            if count = iterations then
+                bitmap.SetPixel(x, y, Color.Black)
             else
-                image.SetPixel(x, y, colorize (count))
+                bitmap.SetPixel(x, y, addColor (count))
     let form = new Form()
-    form.Paint.Add(fun e -> e.Graphics.DrawImage(image, 0, 0))
+    form.Paint.Add(fun e -> e.Graphics.DrawImage(bitmap, 0, 0))
     form
 
 [<STAThread>]
