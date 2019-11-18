471be3325bda1152915b05085cd2a4382de26f09
d4n0n_myself <danon.sibaev@gmail.com>
2019-11-18 12:42:50 +0300

    Singleton

---
+++ SuperDuperSingleton/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace SuperDuperSingleton
+{
+	internal static class Program
+	{
+		private static readonly object lockObj = new object();
+
+		private static void Main(string[] args)
+		{
+			using var streamWriter = System.IO.File.AppendText("test.txt");
+			Parallel.For(0, 10000L, (l, state) =>
+			{
+				var singleton = Singleton.GetInstance("instance" + l);
+				lock (lockObj)
+				{
+					streamWriter.WriteLine(l + " " + singleton.InstanceName);
+				}
+			});
+		}
+	}
+}
---
+++ SuperDuperSingleton/Singleton.cs
@@ -0,0 +1,31 @@
+﻿namespace SuperDuperSingleton
+{
+	internal class Singleton
+	{
+		private Singleton()
+		{
+		}
+
+		private static Singleton _instance;
+
+		private static readonly object Lock = new object();
+
+		public static Singleton GetInstance(string instanceName)
+		{
+			if (_instance == null)
+			{
+				lock (Lock)
+				{
+					if (_instance == null)
+					{
+						_instance = new Singleton {InstanceName = instanceName};
+					}
+				}
+			}
+
+			return _instance;
+		}
+
+		public string InstanceName { get; set; }
+	}
+}
