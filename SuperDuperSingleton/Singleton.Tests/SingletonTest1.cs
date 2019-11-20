4dd4b3cea0dce5f1d82b09c0059cd00b991b3c23
d4n0n_myself <danon.sibaev@gmail.com>
2019-11-20 11:11:56 +0300

    M0Я3 T3$T1ИG

---
+++ SuperDuperSingleton/Singleton.Tests/SingletonTest1.cs
@@ -0,0 +1,45 @@
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace Singleton.Tests
+{
+	public class Tests
+	{
+		private static readonly object lockObj = new object();
+
+		[SetUp]
+		public void Setup()
+		{
+		}
+
+		[Test]
+		public void TryCreate_10000Instances()
+		{
+			System.IO.File.Delete("../../../../inst10000.txt");
+			using var streamWriter = System.IO.File.AppendText("../../../../inst10000.txt");
+			Parallel.For(0, 10000L, (l, state) =>
+			{
+				var singleton = SuperDuperSingleton.Singleton.GetInstance("instance" + l);
+				lock (lockObj)
+				{
+					streamWriter.WriteLine(l + " " + singleton.InstanceName);
+				}
+			});
+		}
+
+		[Test]
+		public void TryCreate_10Instances_WithDebugData()
+		{
+			System.IO.File.Delete("../../../../inst10.txt");
+			using var streamWriter = System.IO.File.AppendText("../../../../inst10.txt");
+			Parallel.For(0, 10, (l, state) =>
+			{
+				var singleton = SuperDuperSingleton.Singleton.TestGetInstance("instance" + l, streamWriter);
+				lock (lockObj)
+				{
+					streamWriter.WriteLine(l + " " + singleton.InstanceName);
+				}
+			});
+		}
+	}
+}
